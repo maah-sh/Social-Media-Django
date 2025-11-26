0094cd553d9e021445145855349ac1fbf2ce3a2c
Mahnoosh <my.lovely.moon.333@gmail.com>
2025-11-26 18:54:39 +0330

    user post_save signal

--- social_media/users/models.py
+++ social_media/users/models.py
@@ -6,8 +6,11 @@ from django.db.models import Q
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
-    image = models.ImageField(upload_to='users/profile_images', blank=True)
+    image = models.ImageField(upload_to='users/profile_images', default='users/profile_images/profile.jpg')
     is_private = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Profile object ({self.pk}) -> User ({self.user.username} : {self.user.first_name} {self.user.last_name})"
 
 
 class Follow(models.Model):
