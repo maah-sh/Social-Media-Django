286c339f9b8382121e1cc3b18bef8b8580edca0d
Mahnoosh <my.lovely.moon.333@gmail.com>
2025-12-03 20:48:31 +0330

    user filters refactored

--- social_media/users/filters.py
+++ social_media/users/filters.py
@@ -1,27 +1,16 @@
 import django_filters.rest_framework as django_filters_rest
 from django.contrib.auth.models import User
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.db import models
-from django.db.models import Q, Value, F
+from django.db.models import Q, Value
 from django.db.models.functions import Concat
 
 
 class UserSearchFilter(django_filters_rest.FilterSet):
-    username__iexact = django_filters_rest.CharFilter(field_name='username', lookup_expr='iexact')
     search = django_filters_rest.CharFilter(method='search_filter')
-    search_full_text = django_filters_rest.CharFilter(method='full_text_search')
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name']
-        filter_overrides = {
-            models.CharField: {
-                'filter_class': django_filters_rest.CharFilter,
-                'extra': lambda f: {
-                    'lookup_expr': 'icontains',
-                },
-            },
-        }
+        fields = ['search']
 
     def search_filter(self, queryset, name, value):
         return queryset.annotate(full_name=Concat('first_name', Value(' '), 'last_name')).filter(
@@ -30,12 +19,6 @@ class UserSearchFilter(django_filters_rest.FilterSet):
             Q(last_name__icontains=value) |
             Q(full_name__icontains=value)
         )
-
-    def full_text_search(self, queryset, name, value):
-        return queryset.alias(
-            vector=SearchVector('username', 'first_name', 'last_name'),
-            query=SearchQuery(value)
-        ).filter(vector=F('query')).annotate(rank=SearchRank(F('vector'), F('query'))).order_by('-rank')
 
 
 
